77564c25530a28167a1600786de631f5a05e67b3
caglatunc <caglatunc03@gmail.com>
2024-01-10 23:20:53 +0300

    EntityFrameworkCore.Auth.WebApi added.

---
+++ EntityFrameworkCore/EntityFrameworkCore.Auth.WebApi/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkCore.Auth.WebApi.Models;
+
+public sealed class AppUser
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+
+}
