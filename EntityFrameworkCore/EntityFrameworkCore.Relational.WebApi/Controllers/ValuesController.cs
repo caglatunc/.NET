6d00c8d4f8b9ab0d5bbf47855ca063519d8797e1
caglatunc <caglatunc03@gmail.com>
2024-01-14 23:14:32 +0300

    Registration process within relations

--- EntityFrameworkCore/EntityFrameworkCore.Relational.WebApi/Controllers/ValuesController.cs
+++ EntityFrameworkCore/EntityFrameworkCore.Relational.WebApi/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
 ﻿using EntityFrameworkCore.Relational.WebApi.Context;
+using EntityFrameworkCore.Relational.WebApi.DTOs;
 using EntityFrameworkCore.Relational.WebApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -15,17 +16,58 @@ public sealed class ValuesController : ControllerBase
         _context = context;
     }
 
-    [HttpGet("Add")]
-    public IActionResult Add(string name)
+    [HttpPost("Add")]
+    public IActionResult Add(CreateProductDto request)
     {
-        Category category = new()
+       
+        Product? product = _context.Products.FirstOrDefault(p => p.Name == request.ProductName);
+
+        if(product is not null)
+        {
+            return BadRequest(new {Message= "Bu isimde bir ürün zaten mevcut!" });
+        }
+
+            product = new()
         {
             Id = Guid.NewGuid(),
-            Name = name,
+            Name = request.ProductName,
+            //CategoryId = category.Id,
         };
 
-        _context.Add(category);
+
+        AdditionalProduct additionalProduct = new()
+        {
+            //ProductId = product.Id,
+            Description = request.ProductDescription,
+            Price = request.ProductPrice,
+        };
+
+        product.AdditionalProduct = additionalProduct;
+
+        Category category = _context.Categories.FirstOrDefault(p => p.Name == request.CategoryName);//Kategori database de kontrol ediyoruz.
+
+        if (category is null)
+        {
+            category = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = request.CategoryName,
+            };
+            //_context.Add(category);
+            product.Category=category;
+        }
+        else
+        {
+            product.CategoryId = category.Id;
+        }
+
+        _context.Add(product);
+
+        //_context.Add(additionalProduct);
+        //_context.Add(category);
+      
         _context.SaveChanges();
-        return Ok(new {Id= category.Id});
+
+        return Ok(new {Id= product.Id});
     }
 }
---
+++ EntityFrameworkCore/EntityFrameworkCore.Relational.WebApi/DTOs/CreateProductDto.cs
@@ -0,0 +1,23 @@
+﻿namespace EntityFrameworkCore.Relational.WebApi.DTOs;
+
+public sealed record CreateProductDto(
+    string ProductName,
+    string ProductDescription,
+    decimal ProductPrice, 
+    string CategoryName);
+
+
+
+//{
+//    public CreateProductDto(string productName, string productDescription, decimal productPrice, string categoryName)
+//    {
+//        ProductName = productName;
+//        ProductDescription = productDescription;
+//        ProductPrice = productPrice;
+//        CategoryName = categoryName;
+//    }
+//    public string ProductName { get; init; }
+//    public string ProductDescription { get; init; }
+//    public decimal ProductPrice { get; init; }
+//    public string CategoryName { get; init; }
+//}
