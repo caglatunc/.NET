fe419ad32d432238af74810e858af21928bbd66c
caglatunc <caglatunc03@gmail.com>
2024-04-01 23:40:44 +0300

    RabbitMQ added.

---
+++ RabbitMQ/2.WorkQueues/Worker/Program.cs
@@ -0,0 +1,12 @@
+﻿using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "task_queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
